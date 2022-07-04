4d5a5caefb1e2dbad7bf647a7cd6c95be70155ae
rajadoanggg <rajadoangbang@gmail.com>
2022-07-04 07:56:31 +0700

    new sc

---
+++ admin/welcome.js
@@ -0,0 +1,9 @@
+exports.welcome1 = (tag) => {
+return `[ WELCOME TO , JANGAN LUPA BACA DESKRIPSI KAWAN ]
+USER : ${tag}`
+}
+
+exports.goodbye1 = (tag) => {
+return `[ *SELAMAT TINGGAL:( BY BEBAN HAHAHA ]
+USER : ${tag}`
+}
